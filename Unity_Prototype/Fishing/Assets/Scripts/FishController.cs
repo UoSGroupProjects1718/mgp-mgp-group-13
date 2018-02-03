54231a46c757f671b3290556204871fcdc947457
err0rzzz <s190988@uos.ac.uk>
2018-02-03 20:07:15 +0000

    first prototype
    prefab for fish / spawn strips, animation in place for line.

---
+++ Unity_Prototype/Fishing/Assets/Scripts/FishController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FishController : MonoBehaviour {
+
+    public float speed, direction;
+    
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        Vector3 tickMove = new Vector3(1 * speed * direction * Time.deltaTime, 0.0f, 0.0f);
+        transform.position += tickMove;
+	}
+}
---
+++ Unity_Prototype/Fishing/Assets/Scripts/FishStripController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FishStripController : MonoBehaviour {
+
+    public float SpawnRate;
+    private float SpawnTick;
+    public GameObject fish;
+    public Transform SpawnPoint;
+    
+    // Use this for initialization
+    void Start()
+    {
+        SpawnTick = SpawnRate;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (SpawnTick > 0)
+        {
+            SpawnTick -= Time.deltaTime;
+        }
+
+        else if (SpawnTick <= 0)
+        {
+            Instantiate(fish, SpawnPoint);
+            SpawnTick = SpawnRate;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("fish"))
+        {
+            other.gameObject.SetActive(false);
+        }
+    }
+}
