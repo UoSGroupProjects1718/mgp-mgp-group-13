c321ca3a4c7db92d5108c70d0424a17fef28131a
err0rzzz <s190988@uos.ac.uk>
2018-02-25 17:06:53 +0000

    created and animated level timer

--- Unity_Prototype/Fishing/Assets/Scripts/Timer.cs
+++ Unity_Prototype/Fishing/Assets/Scripts/Timer.cs
@@ -6,28 +6,37 @@ using UnityEngine;
 public class Timer : MonoBehaviour {
 
     public float RoundTime;
+    public GameObject marker;
 
-	// Use this for initialization
-	void Start () {
-		
-        
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    public Transform startMarker;
+    public Transform endMarker;
 
+    private float initialTime;
+
+
+    void Start()
+    {
+        initialTime = RoundTime;
+    }
+
+
+    void Update()
+    {
+        // counts down
+        if (RoundTime > 0)
         {
-            // counts down
-            if (RoundTime > 0)
-            {
-                RoundTime -= Time.deltaTime;
-            }
-
-            else if (RoundTime <= 0) // when counter reaches zero call end the level
-            {
-               // some code to make the level end and display final scores
-            }
+            RoundTime -= Time.deltaTime;
         }
 
+        else if (RoundTime <= 0) // when counter reaches zero call end the level
+        {
+            // some code to make the level end and display final scores
+        }
+
+
+
+        float alpha = RoundTime / initialTime;
+        marker.transform.position = Vector3.Lerp(endMarker.position, startMarker.position, alpha);
     }
+
 }
