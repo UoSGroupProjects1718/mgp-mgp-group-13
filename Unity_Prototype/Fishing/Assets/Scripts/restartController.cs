c797a8d8f43a3e4802a5dd7c2ee6d550dede83e4
Alex M <s190988@uos.ac.uk>
2018-03-08 15:42:28 +0000

    added keyboard reset buttons for menus, working
    tested, does not toggle back, once ready, stays ready

--- Unity_Prototype/Fishing/Assets/Scripts/restartController.cs
+++ Unity_Prototype/Fishing/Assets/Scripts/restartController.cs
@@ -30,8 +30,33 @@ public class restartController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update ()
+
     {
 
+        // gives p1 an input button which will interact with thier toggle and flip flop it
+        if (Input.GetKey(KeyCode.LeftControl) == true)   
+        {
+          //  if (p1.isOn == false)
+                p1.isOn = true;
+
+           // if (p1.isOn == true)
+           //     p1.isOn = false;
+        }
+
+        // gives p2 an input button which will interact with thier toggle and flip flop it
+        if (Input.GetKeyDown(KeyCode.RightControl) == true)
+        {
+          //  if (p2.isOn == false)
+                p2.isOn = true;
+
+          //  if (p2.isOn == true)
+           //     p2.isOn = false;
+        }
+
+
+
+
+        //when both players are ready the game begins
         if (p1.isOn == true & p2.isOn == true)
         {
             SceneManager.LoadScene("main", LoadSceneMode.Single);
