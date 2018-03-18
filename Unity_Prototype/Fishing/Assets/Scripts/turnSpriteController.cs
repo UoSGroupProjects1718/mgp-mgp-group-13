0fdef49ae8da4d842980c73919a78810930af5d4
err0rzzz <s190988@uos.ac.uk>
2018-03-18 03:06:22 +0000

    added line and power up fish assets to scene
    line also alternates based on who's turn it is

--- Unity_Prototype/Fishing/Assets/Scripts/turnSpriteController.cs
+++ Unity_Prototype/Fishing/Assets/Scripts/turnSpriteController.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class turnSpriteController : MonoBehaviour {
 
-    public SpriteRenderer p1bkg, p2bkg;
+    public SpriteRenderer p1Sprite, p2Sprite;
     public Sprite activeSprite, inactiveSprite;
 
     public GameObject gamecontroller;
@@ -12,8 +12,8 @@ public class turnSpriteController : MonoBehaviour {
     // Use this for initialization
     void Start ()
     {
-        p1bkg.sprite = activeSprite;
-        p2bkg.sprite = inactiveSprite;
+        p1Sprite.sprite = activeSprite;
+        p2Sprite.sprite = inactiveSprite;
 
     }
 	
@@ -25,14 +25,14 @@ public class turnSpriteController : MonoBehaviour {
         
         if (controller.IsP1Active() == true)
         {
-            p1bkg.sprite = activeSprite;
-            p2bkg.sprite = inactiveSprite;
+            p1Sprite.sprite = activeSprite;
+            p2Sprite.sprite = inactiveSprite;
         }
 
         else
         {
-            p2bkg.sprite = activeSprite;
-            p1bkg.sprite = inactiveSprite;
+            p1Sprite.sprite = inactiveSprite;
+            p2Sprite.sprite = activeSprite;
         }
 	}
 }
