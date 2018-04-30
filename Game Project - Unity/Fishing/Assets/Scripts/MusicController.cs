7bdc288250fb7d55651463738eabbd4344d6412b
amyloupotter <s183169@ucs.ac.uk>
2018-04-30 11:05:19 +0100

    Fixed music duplication issue
    Music controller can be altered if alternate menu music gets made. Currently works to run single theme through all scenes.

--- Game Project - Unity/Fishing/Assets/Scripts/MusicController.cs
+++ Game Project - Unity/Fishing/Assets/Scripts/MusicController.cs
@@ -3,9 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class MusicController : MonoBehaviour {
+    public static MusicController instance;
 
-	// Use this for initialization
-	void Start () {
-		DontDestroyOnLoad(this.gameObject); //prevents MusicPlayer game object from being destroyed so that the waves music is played consistently throughout each scene 
-	}
+    // Use this for initialization
+    void Start()
+    {
+        if (instance == null)
+            instance = this;
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        DontDestroyOnLoad(this.gameObject); //prevents MusicPlayer game object from being destroyed so that the waves music is played consistently throughout each scene 
+    }
 }
+
